6ab7c231c019e66cd07a2a286bab9e14987fba02
shajahan-sayed <shajahansayed111@gmail.com>
2025-12-15 19:32:40 +0530

    Create HelloController.java

---
+++ src/main/java/com/example/jenkinscicd/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.jenkinscicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to Jenkins CI/CD!";
+    }
+}
