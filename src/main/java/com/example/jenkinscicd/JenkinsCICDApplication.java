d6076da8128ef219479850133f7545fe9a59e99b
shajahan-sayed <shajahansayed111@gmail.com>
2025-12-15 19:31:55 +0530

    Create JenkinsCICDApplication.java

---
+++ src/main/java/com/example/jenkinscicd/JenkinsCICDApplication.java
@@ -0,0 +1,11 @@
+package com.example.jenkinscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCICDApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCICDApplication.class, args);
+    }
+}
